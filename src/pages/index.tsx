2a954ca8811a41229b66ea86a87c47445c94e186
mvfflin <mynameisfatihul@gmail.com>
2024-06-04 19:48:45 +0700

    updated colors and links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,55 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import PrototypeImage from "../assets/prototype.jpeg";
+import TextTransition, { presets } from "react-text-transition";
+import { useEffect, useState } from "react";
 // import FoodCover from "../assets/cover-makanan.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const HomeTexts = [
+    "Jaga Lingkungan Mulai Dari Menghabiskan Makanan",
+    "Kurangi Limbah Sisa Makanan",
+    "Ayo Habiskan Makananmu!",
+];
+
 export default function Home() {
+    const [textindex, setTextIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(
+            () => setTextIndex((index) => index + 1),
+            5000 // every 3 seconds
+        );
+        return () => clearTimeout(intervalId);
+    }, []);
+
     return (
         <main>
             <section
                 id="home"
-                className="cover-makanan h-screen w-full bg-cover bg-no-repeat bg-center bg-blend-multiply text-center py-20"
+                className="cover-makanan h-screen w-full bg-cover bg-no-repeat bg-center bg-blend-multiply text-center md:py-[350px] py-60 px-20"
                 // style={{ backgroundImage: `url(${FoodCover})` }}
             >
-                <h2 className="lg:text-4xl text-2xl font-bold mb-4 mt-32 text-white font-serif lg:px-72">
-                    Jaga Lingkungan Mulai Dari Menghabiskan Makanan
+                <h2 className="lg:text-4xl text-xl font-bold mb-4 text-white font-serif">
+                    {HomeTexts[0]}
                 </h2>
-                <hr className="mb-8 text-white w-72 mx-auto"></hr>
+                <hr className="mb-8 text-white lg:w-96 w-auto mx-auto"></hr>
                 <a
                     href="#about"
-                    className="text-lg hover:text-xl bg-white bg-opacity-0 hover:bg-opacity-100 border-2 border-white hover:text-black text-white py-2 px-4"
+                    className="lg:text-lg lg:hover:text-xl bg-white bg-opacity-0 hover:bg-opacity-100 border-2 border-white hover:text-black text-white py-2 px-4"
                 >
                     Explore More
                 </a>
             </section>
-            <section id="about" className="bg-green-100 h-full w-full p-20">
+            <section id="about" className="bg-emerald-100 h-full w-full p-20">
                 <div className="lg:inline-flex">
                     <Image
                         alt="prototype"
                         src={PrototypeImage}
                         className="lg:h-[400px] lg:w-[400px] w-60 h-60"
                     />
-                    <h2 className="lg:inline-block lg:ml-20 lg:text-justify lg:text-xl my-20">
+                    <h2 className="lg:inline-block lg:ml-20 text-justify my-7 lg:text-xl lg:my-32">
                         Rasa Tak Terbuang bukan sekadar aplikasi jual beli
                         makanan biasa. Aplikasi ini memberikan platform bagi
                         para penjual makanan untuk menawarkan sisa makanan
